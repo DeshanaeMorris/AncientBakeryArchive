8f40854700a5a788d020096094c310e32e3ab87f
DeshanaeMorris <morrisdeshanae@gmail.com>
2026-06-07 21:21:31 -0400

    Changes based on ERD

---
+++ AncientBakery/src/model/Era.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Era {
+    private int id;
+    private String name;
+    private String timePeriod;
+    private String historyText;
+
+    public Era() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTimePeriod() {
+        return timePeriod;
+    }
+
+    public void setTimePeriod(String timePeriod) {
+        this.timePeriod = timePeriod;
+    }
+}
+public String getHistoryText() {
+    return historyText;
+}
+
+public void setHistoryText(String historyText) {
+    this.historyText = historyText;
+}
